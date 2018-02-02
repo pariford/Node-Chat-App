e59b1bc45d8ce2ae9f0f9ea3a11c21651d93ae34
Paritosh <pabapat@paypal.com>
2018-02-02 12:34:43 +0530

    "Create addMessage and newMessage"

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+//it will create a web socket connection
+var socket = io();
+socket.on('connect', function () {
+    console.log("Connected to server");
+
+/*     socket.emit('createEmail', {
+        to: "shivanics.vit@gmail.com",
+        text: "Hey..This is Paritosh"
+    }); */
+
+    socket.emit('createMessage', {
+        to: "Shivani",
+        text: "Hey..This is Paritosh"
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log("Diconnected from the server")
+});
+
+//the arguments here takes the object passed by the emit function
+/* socket.on('newEmail', function (email) {
+    console.log("New email", email);
+}); */
+
+socket.on('newMessage', function (message) {
+    console.log("New message", message);
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,52 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 var app = express();
 const publicPath = path.join(__dirname, '../public');
 //to support the express middleware and connection pooling with the viewa
 app.use(express.static(publicPath));
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+
+var server = http.createServer(app);
+//configure the server to use socket io
+var io = socketIO(server);
+//it will register an event listener for any new connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    //non-listener doesn't requires a callback function.If you want to send data
+    //provide a second argument.It can be an object as well
+/*     socket.emit('newEmail', {
+        from: "paritoshvit@gmail.com",
+        text: "Hi,What's up?",
+        createdAt: new Date().getTime()
+    }); */
+
+    socket.emit('newMessage', {
+        from: "Paritosh",
+        text: "Hi,What's up?",
+        createdAt: new Date().getTime()
+    });
+
+    /* socket.on('createEmail', (newEmail) => {
+        console.log("Create Email", newEmail);
+    }); */
+
+    socket.on('createMessage', (newMessage) => {
+        newMessage.createdAt = new Date().getTime();
+        console.log("Create Message", newMessage);
+    });
+
+    //listener requires a callback function
+    socket.on('disconnect', () => {
+        console.log("Browser/Client is closed");
+    })
+})
+server.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+/* app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+}); */
